96b75d8530b20c72455b5ceb8ca2501a9900a9be
PedrorOliva <pedro.robson@zup.com.br>
2023-09-18 18:56:56 -0300

    subindo projeto

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_donor (name, email) VALUES ('Pedro', 'pedro@email.com')
+INSERT INTO tb_campaign(name, description, goal, start, finish) VALUES ('Campanha', 'Teste H2', '150.000',
+'18/09/2023', '28/11/2023')
+INSERT INTO tb_donation (value, donationDate, paymentMethod, donor, campaign) VALUES ('13.00',
+'18/09/2024', 'dinheiro')
