b0423873746d5359ca954df1bf339d50acfecf48
PedrorOliva <pedro.robson@zup.com.br>
2023-09-18 18:58:54 -0300

    subindo projeto

---
+++ src/test/java/com/catalisa/GerenciamentoDoacoes/GerenciamentoDoacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalisa.GerenciamentoDoacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDoacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
